7caf16b472a9f5d209a6da19f8ad728c596d0310
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-07-09 10:47:22 +0530

    #include<stdio.h> int main() {     int n;     int dig,revNumber;     scanf("%d",&n);     revNumber=0;     while(n>0)     {         dig=n%10;         revNumber=(revNumber*10)+dig;         n=n/10;     }     printf("%d ",revNumber);     return 0; }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int d,n,i,res=0;
+    int n;
+    int dig,revNumber;
     scanf("%d",&n);
-    while(n!=0)
+    revNumber=0;
+    while(n>0)
     {
-        d=n%10;
+        dig=n%10;
+        revNumber=(revNumber*10)+dig;
         n=n/10;
-        res=res*10+d;
-        
     }
-    printf("%d",res);
+    printf("%d
+",revNumber);
     return 0;
 }
