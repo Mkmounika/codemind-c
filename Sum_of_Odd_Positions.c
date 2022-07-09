59b88ff6329c7c4f99c57cca3bc7430f00715f73
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-07-09 15:42:23 +0530

    #include<stdio.h> int main() {     int i,n,s=0,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             s=s+a[i];         }     }     printf("%d",s); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,s=0,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
