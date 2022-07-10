9f431df3626a98f2cbddd8d3e528b38f8f534316
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-07-10 21:59:14 +0530

    #include<stdio.h> int main() {    char str[100],i;    int upper = 0, lower = 0, number = 0, special = 0,whitesp=0;    scanf("%[^ ]s",str);    for (i = 0; i < str[i]!=NULL; i++)    {       if (str[i] >= 'A' && str[i] <= 'Z')          upper++;       else if (str[i] >= 'a' && str[i] <= 'z')          lower++;       else if (str[i]>= '0' && str[i]<= '9')          number++;       else if(str[i]==' ')          whitesp++;       else          special++;    }    printf("%d ",upper); }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   char str[100],i;
+   int upper = 0, lower = 0, number = 0, special = 0,whitesp=0;
+   scanf("%[^
+]s",str);
+   for (i = 0; i < str[i]!=NULL; i++)
+   {
+      if (str[i] >= 'A' && str[i] <= 'Z')
+         upper++;
+      else if (str[i] >= 'a' && str[i] <= 'z')
+         lower++;
+      else if (str[i]>= '0' && str[i]<= '9')
+         number++;
+      else if(str[i]==' ')
+         whitesp++;
+      else
+         special++;
+   }
+   printf("%d ",upper);
+}
