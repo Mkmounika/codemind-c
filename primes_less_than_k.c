ee1b82bffccfc2aa469a9831aa268a5c86bdfba0
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-07-10 12:50:31 +0530

    #include<stdio.h> int main() {     int n,i,j,s,x,m=0,k=0,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&m);     for(i=x;i<=n;i++)     {         s=0;         if(a[i]<=m)         for(j=1;j<a[i];j++)         {             if(a[i]%j==0)             {                 s++;             }         }         if(s==1)         {             k++;         }     }     printf("%d",k); }

---
+++ primes_less_than_k.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,s,x,m=0,k=0,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&m);
+    for(i=x;i<=n;i++)
+    {
+        s=0;
+        if(a[i]<=m)
+        for(j=1;j<a[i];j++)
+        {
+            if(a[i]%j==0)
+            {
+                s++;
+            }
+        }
+        if(s==1)
+        {
+            k++;
+        }
+    }
+    printf("%d",k);
+}
